9997eae908a283ffa1ebd395daeb49ecdc72ca82
marwa-dev <dhaouadimarwa12@outlook.fr>
2020-09-27 14:06:54 +0100

    first project

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="/login.css"/>
+</head>
+<body>
+<div class="container">
+  <form class="form">
+  <label>Username</label>
+     <input type="text" name="username"><br>
+  <label>Password</label>
+    <input type="password" name="password"><br>
+  <button type="submit">Login </button>
+<label>
+</div>
+    
+</body>
+</html>
